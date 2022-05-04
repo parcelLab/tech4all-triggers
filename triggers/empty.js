c78131041eb7207c39ff0bbac89f6d686d9c4123
Alioun <14974659+Alioun@users.noreply.github.com>
2022-05-04 16:42:28 +0200

    formatting and typofix

--- triggers/empty.js
+++ triggers/empty.js
@@ -1,58 +1,58 @@
 
-  /* ------------------------------ Recipient slots ----------------------------- */
-  
-  const toRecipient = { customer: true, support: false, test: false, plTest: false, courierContact: true }
-  // const webhookRecipient = { theirs: true }
-  
-  /* ------------------------------ Trigger slots ----------------------------- *
-   * These are the groups a trigger can be .push()ed into to activate it.
-   */
-  
-  const onOrderCreated = []
-  const onOrderUpdated = []
-  const onTrackingCreated = []
-  const onTrackingUpdated = []
-  
-  // see watchlist types
-  const onInvalid = []
-  const onForgotten = []
-  
-  // happy flow
-  const onDispatch = []
-  const onDeliveryStatus = []
-  const onScheduled = []
-  const onPickupReady = []
-  const onReturnWarning = []
-  const onDelivered = []
-  
-  // sad flow
-  const onDelay = []
-  const onException = []
-  const onEscalation = []
-  const onReturn = []
-  
-  const onCustomEvent = [] // special additional events
-  const onTrackingClosed = []
-    
-  /* -------------------------------------------------------------------------- */
-  /*                         Trigger definitions go here                        */
-  /* -------------------------------------------------------------------------- */
+/* ------------------------------ Recipient slots ----------------------------- */
 
-  onTrackingCreated.push({
+const toRecipient = { customer: true, support: false, test: false, plTest: false, courierContact: true }
+// const webhookRecipient = { theirs: true }
+
+/* ------------------------------ Trigger slots ----------------------------- *
+ * These are the groups a trigger can be .push()ed into to activate it.
+ */
+
+const onOrderCreated = []
+const onOrderUpdated = []
+const onTrackingCreated = []
+const onTrackingUpdated = []
+
+// see watchlist types
+const onInvalid = []
+const onForgotten = []
+
+// happy flow
+const onDispatch = []
+const onDeliveryStatus = []
+const onScheduled = []
+const onPickupReady = []
+const onReturnWarning = []
+const onDelivered = []
+
+// sad flow
+const onDelay = []
+const onException = []
+const onEscalation = []
+const onReturn = []
+
+const onCustomEvent = [] // special additional events
+const onTrackingClosed = []
+
+/* -------------------------------------------------------------------------- */
+/*                         Trigger definitions go here                        */
+/* -------------------------------------------------------------------------- */
+
+onTrackingCreated.push({
     Trigger: '*',
     Channel: 'Mail',
     Message: 'DispatchConfirmation',
     LocaleMessage: true,
     Delay: 0,
-    Recipient: onlyTesting,
-    Filter: { }
-  })
+    Recipient: toRecipient,
+    Filter: {}
+})
 
-  /* -------------------------------------------------------------------------- */
-  /*                  Export and custom validations if required                 */
-  /* -------------------------------------------------------------------------- */
-  
-  module.exports = {
+/* -------------------------------------------------------------------------- */
+/*                  Export and custom validations if required                 */
+/* -------------------------------------------------------------------------- */
+
+module.exports = {
     onOrderCreated,
     onOrderUpdated,
     onTrackingCreated,
@@ -71,5 +71,4 @@
     onReturn,
     onCustomEvent,
     onTrackingClosed
-  }
-  
+}
